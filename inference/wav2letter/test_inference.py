33503b9de71143a89cd4f8af1254ddb4318a3c27
Tetiana Myronivska <tetiana@notablehealth.com>
2020-11-23 15:25:21 -0500

    update paths

--- inference/wav2letter/test_inference.py
+++ inference/wav2letter/test_inference.py
@@ -7,14 +7,14 @@ def print_result(result):
     )
 
 
-model = inference.load_model(input_files_base_path="/root/model")
+model = inference.load_model(
+    input_files_base_path="/home/tetianamyronivska/tds_ctc_streaming_serialized"
+)
 inference_stream = model.open_stream()
 
 chunk_size = 32000  # 32000 = 1 sec (mobile sends chunks of 3200)
 
-with open(
-    "/root/audio/LibriSpeech/dev-clean/777/126732/777-126732-0051.flac.wav", "rb"
-) as f:
+with open("/home/tetianamyronivska/tds_ctc_streaming_serialized/r1.wav", "rb") as f:
     f.seek(44)  # skip WAV header
     bytes = f.read(chunk_size)
     while bytes:
