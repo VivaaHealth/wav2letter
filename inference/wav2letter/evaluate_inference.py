bf901261df5ae0b8062f72ffcd0866fc2e827062
Tetiana Myronivska <tetiana@notablehealth.com>
2020-11-25 08:25:48 -0500

    Update evaluate_inference.py

--- inference/wav2letter/evaluate_inference.py
+++ inference/wav2letter/evaluate_inference.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from pathlib import Path
 from wav2letter import inference
 
@@ -16,25 +18,28 @@ local_dir = "/home/tetianamyronivska/test_segments_by_provider"
 providers = ["diane_snyder"]
 
 for provider in providers:
-    path = Path(f"{local_dir}/{provider}")
-    for p in path.rglob("*"):
-        with open(p, "rb") as f:
-            print(p)
-            print(p.suffix)
-            inference_stream = model.open_stream()
-            f.seek(44)  # skip WAV header
-            bytes = f.read(chunk_size)
-            try:
-                while bytes:
-                    inference_stream.submit_audio(bytes)
-                    result = inference_stream.next_result()
-                    # inference_stream.prune()
-                    bytes = f.read(chunk_size)
+    test_file_path = Path(f"{local_dir}/{provider}/test.tsv")
+    test_df = pd.read_csv(test_file_path)
+    audio_ids = test_df["id"].tolist()
+    print(audio_ids)
+    # for p in path.rglob("*"):
+    #     with open(p, "rb") as f:
+    #         print(p)
+    #         if p.suffix == ".wav":
+    #             inference_stream = model.open_stream()
+    #             f.seek(44)  # skip WAV header
+    #             bytes = f.read(chunk_size)
+    #             try:
+    #                 while bytes:
+    #                     inference_stream.submit_audio(bytes)
+    #                     result = inference_stream.next_result()
+    #                     # inference_stream.prune()
+    #                     bytes = f.read(chunk_size)
 
-                inference_stream.end_audio()
-                result = inference_stream.next_result()
-                inference_stream.prune()
-                result = " ".join([w.word for w in result.words])
-                print(result)
-            except RuntimeError:
-                continue
+    #                 inference_stream.end_audio()
+    #                 result = inference_stream.next_result()
+    #                 inference_stream.prune()
+    #                 result = " ".join([w.word for w in result.words])
+    #                 print(result)
+    #             except RuntimeError:
+    #                 continue
