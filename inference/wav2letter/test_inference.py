b3d1f4a5e5bb553114b070644cb06c41aa0a3fc1
Tetiana Myronivska <tetiana@notablehealth.com>
2020-12-27 22:13:53 -0500

    revert to master

--- inference/wav2letter/test_inference.py
+++ inference/wav2letter/test_inference.py
@@ -1,47 +1,30 @@
-from timeit import default_timer as timer
 from wav2letter import inference
 
 
 def print_result(result):
     print(
-        f"{result.chunk_start_time}-{result.chunk_end_time} ms: {[f'{w.word}_{w.begin_time_frame}-{w.end_time_frame}' for w in result.words]}"
+        f"{result.chunk_start_time}-{result.chunk_end_time} ms: {[f'{w.word}({w.begin_time_frame})' for w in result.words]}"
     )
 
 
-# model = inference.load_model(
-#     input_files_base_path="/local/working/dir/2020_11_25_tds_ctc_serialized"
-# )
-model = inference.load_model(
-    input_files_base_path="/local/working/dir/2020_11_25_tds_ctc_serialized"
-)
+model = inference.load_model(input_files_base_path="/root/model")
 inference_stream = model.open_stream()
 
-chunk_size = 8000  # 32000 = 1 sec (mobile sends chunks of 3200)
+chunk_size = 32000  # 32000 = 1 sec (mobile sends chunks of 3200)
 
 with open(
-    "/home/tetianamyronivska/test_segments_by_provider/aaron_dickens/40e9f3ac-b1b8-417d-a41c-a097ee8a5400_0.wav",
-    # "/home/tetianamyronivska/test_segments_by_provider/aaron_dickens/e9d9ad26-24fd-411d-b375-6344899229a2_1.wav",
-    # "/home/tetianamyronivska/2020-12-02T19_57_02.334Z.wav",
-    "rb",
+    "/root/audio/LibriSpeech/dev-clean/777/126732/777-126732-0051.flac.wav", "rb"
 ) as f:
-    start = timer()
     f.seek(44)  # skip WAV header
     bytes = f.read(chunk_size)
     while bytes:
         inference_stream.submit_audio(bytes)
         result = inference_stream.next_result()
         # inference_stream.prune()
-        # print(result)
-        # print_result(result)
+        print_result(result)
         bytes = f.read(chunk_size)
 
 inference_stream.end_audio()
 result = inference_stream.next_result()
-end = timer()
-print(f"Transcripttion took {end - start} seconds")
-print(" ".join([w.word for w in result.words]))
-print("\n")
 inference_stream.prune()
-# print_result(result)
-print(" ".join([w.word for w in result.words]))
-
+print_result(result)
